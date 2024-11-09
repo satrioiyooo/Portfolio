7dbe55bdef10d4954928584822540ee068575865
satrioiyooo <satriowibowo004@gmail.com>
2024-11-09 14:36:45 +0700

    env

--- server.js
+++ server.js
@@ -40,5 +40,5 @@ app.get("*", (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server running on port http://localhost:${PORT} `);
+  console.log(`Server running on http://localhost:${PORT}`);
 });
